8bb3586e6211508ef1c4d5e13070f2cb690c8204
Aaron Aguilar-Chavez <oyorom8@gmail.com>
2023-02-21 12:01:56 -0600

    break for lunch

--- client.js
+++ client.js
@@ -61,22 +61,33 @@ const employees = [
 // * The `totalCompensation` property should be the adjusted annual compensation (base annual + bonus)
 // * The `totalBonus` should be the employee's total bonus rounded to the nearest dollar.
 
+// - Those who have a rating of a 2 or below should not receive a bonus.
+// - Those who have a rating of a 3 should receive a base bonus of 4% of their base annual income.
+// - Those who have a rating of a 4 should receive a base bonus of 6% of their base annual income.
+// - Those who have a rating of a 5 should receive a base bonus of 10% of their base annual income.
+// - If their employee number is 4 digits long, this means they have been with the company for longer than 15 years,
+// and should receive an additional 5%.
+// - However, if their annual income is greater than $65,000, they should have their bonus adjusted down 1%.
+// - No bonus can be above 13% or below 0% total.
 
 
 
 // This function will calculate 1 employee's bonus!
 //
-function calculateIndividualEmployeeBonus( employee ) {  //this employee parameter should be an object of employee array
+function calculateIndividualEmployeeBonus(employee) {  //this employee parameter should be an object of employee array
   const bonusDetails = [];
-    for(let person of employees){
-      if (person.name === employee){                     //checking for a match withing employees array
-          console.log('You have found a match!:', employee);
-          bonusDetails.push({name:person.name, bonusPercentage: 'testPercentage' , totalCompensation:'testCompensation' , totalBous:'testBonus' });
-          console.log('bonus detals', bonusDetails);  //testing new object creation with required properties
+  for (let person of employees) {
+    if (person.name === employee) {                     //checking for a match withing employees array
+      console.log('You have found a match!:', employee);
+      if (person.reviewRating = 3) {
+        console.log('review rating is equal to 3');
       }
+      bonusDetails.push({ name: person.name, bonusPercentage: 'testPercentage', totalCompensation: 'testCompensation', totalBous: 'testBonus' });
+      console.log('bonus detals', bonusDetails);  //testing new object creation with required properties
     }
-  
-  
+  }
+
+
   // return new object with bonus results
   return
 }
